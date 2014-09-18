f94dca6678730ed753d610c4abec0d874db03497
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-17 18:29:57 -0700

    Renaming some text files
    --HG--
    branch : GSRedirect-newstyle
    rename : README.txt => README.rst
    rename : docs/HISTORY.txt => docs/HISTORY.rst

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-import os
 from setuptools import setup, find_packages
+import codecs
+import os
 from version import get_version
 
-version = get_version()
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
 
+version = get_version()
 setup(name='Products.GSRedirect',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -30,14 +35,14 @@ setup(name='Products.GSRedirect',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
       ],
       keywords='post, topic, image, file, redirect',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
-      url='http://groupserver.org',
+      url='https://github.com/groupserver/Products.GSRedirect/',
       license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
